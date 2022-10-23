964275418b332699ac08e63ae5d2a7b247bc75dd
jeovanedossantossantos <jeovane.ssantos@outlook.com>
2022-10-23 10:23:30 -0300

    chore: finshed module

--- coderCursos/navegacao/src/routes/Tab.js
+++ coderCursos/navegacao/src/routes/Tab.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { createStackNavigator } from '@react-navigation/stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import TelaA from '../views/TelaA/TelaA';
 import TelaB from '../views/TelaB/TelaB'
 import TelaC from '../views/TelaC/TelaC'
@@ -14,12 +15,46 @@ export default StackNavigator = (props) => {
 
     return (
         <Tab.Navigator
-            screenOptions={{
-                activeTintColor: "red",
-                InactiveTintColor: "blue",
-                labelStyle: { fontSize: 30 }
-            }}
+            screenOptions={
+
+
+
+                ({ route }) => ({
+                    activeTintColor: 'red',
+                    inactiveTintColor: 'blue',
+                    showLabel: false,
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+
+                        switch (route.name) {
+                            case 'TelaA':
+                                iconName = focused
+                                    ? 'ios-information-circle'
+                                    : 'ios-information-circle-outline';
+                                break;
+
+                            case 'TelaB':
+                                iconName = focused
+                                    ? 'ios-information-circle'
+                                    : 'ios-information-circle-outline';
+                                break;
+
+                            case 'TelaC':
+                                iconName = focused ? 'ios-list-circle-sharp' : 'ios-list-circle-outline';
+                                break;
+
+                            default:
+                                break;
+                        }
+
+
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                    tabBarActiveTintColor: 'tomato',
+                    tabBarInactiveTintColor: 'gray',
+                })}
             initialRouteName="TelaB"
+
 
         >
             <Tab.Screen name="TelaA" component={TelaA} />
